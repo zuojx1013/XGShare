adaf973406915784176f456da6892dde08f37e7a
mac <mac@macdeMac-mini.local>
2015-08-25 09:50:23 +0800

    Initial Commit

---
+++ share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  share
+//
+//  Created by mac on 15/8/25.
+//  Copyright (c) 2015年 xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ share/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  share
+//
+//  Created by mac on 15/8/25.
+//  Copyright (c) 2015年 xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
