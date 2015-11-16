5924a21e388cda47579006f10b0ec5f8eded28b3
mac <mac@macdeMac-mini.local>
2015-11-16 14:39:46 +0800

    merge

--- share/ViewController.m
+++ share/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "UMSocial.h"
 
-@interface ViewController ()
+@interface ViewController ()<UMSocialUIDelegate>
 
 @end
 
@@ -17,6 +18,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(10, 10, 80, 60);
+    btn.backgroundColor = [UIColor redColor];
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+
+- (void)btnClick:(UIButton *)sender{
+    
+    [UMSocialData defaultData].extConfig.qqData.shareText = @"分享到新浪微博内容1";
+    [UMSocialData defaultData].extConfig.qqData.url = @"http://www.baidu.com";
+    NSString *shareText = @"友盟社会化组件可以让移动应用快速具备社会化分享、登录、评论、喜欢等功能，并提供实时、全面的社会化数据统计分析服务。 http://www.umeng.com/social";             //分享内嵌文字
+    UIImage *shareImage = [UIImage imageNamed:@"UMS_social_demo"];          //分享内嵌图片
+    
+    //调用快速分享接口
+    [UMSocialSnsService presentSnsIconSheetView:self
+                                         appKey:@"53290df956240b6b4a0084b3"
+                                      shareText:shareText
+                                     shareImage:shareImage
+                                shareToSnsNames:nil
+                                       delegate:self];
 }
 
 - (void)didReceiveMemoryWarning {
